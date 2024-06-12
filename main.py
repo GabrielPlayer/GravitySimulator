1361ebf87e5bca07e9464165b96ec4d815559089
Gabriel <teigne.gabriel@gmail.com>
2024-06-12 22:15:53 +0200

    Add gravity attraction

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Main:
 
         self.planetsGroup: list[Planet] = []
         self.planetsGroup.append(Planet(pos=(0,0), mass=2e30, vel=(0,0), color=(200,0,100)))
+        self.planetsGroup.append(Planet(pos=(1,0), mass=2e30, vel=(0,0), color=(200,0,100)))
 
         self.backgroundColor = (0,0,0)
         self.isRun = False
@@ -24,6 +25,9 @@ class Main:
             for event in events:
                 if event.type == pygame.QUIT:
                     self.isRun = False
+
+            for p in self.planetsGroup:
+                p.update(self.planetsGroup)
 
             for p in self.planetsGroup:
                 p.draw(self.screen)
--- planet.py
+++ planet.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import pygame
+import math
 
 class Planet:
     AU = 149.6e6 * 1000
@@ -10,15 +11,30 @@ class Planet:
     HEIGHT = 800
 
     def __init__(self, pos: tuple[int, int], mass: int, vel: tuple[int,int], color=(25,112,219)) -> None:
-        """ pos is base on the center of the window\n
+        """ pos in AU (earth is at 1 AU of the sun) is base on the center of the window\n
             mass in kg (earth mass is 6e26 and sun mass is 2e30)\n
-            vel is the x and y base speed of the planet
+            vel in km/s is the x and y base speed of the planet
         """
         self.color = color
-        self.pos = pos
+        self.pos = (pos[0]*self.AU, pos[1]*self.AU)
         self.mass = mass
-        self.vel = vel
+        self.vel = (vel[0]*1000, vel[1]*1000)
         self.radius = 5 # TODO: add radius base on the mass and the window's scale
+
+    def attraction(self, planet: Planet) -> tuple[float, float]:
+        x,y = planet.pos[0]-self.pos[0],planet.pos[1]-self.pos[1]
+        d = math.sqrt(x**2+y**2)
+        force = self.G*(planet.mass*self.mass)/(d**2)
+        theta = math.atan2(y,x)
+        fX, fY = math.cos(theta) * force, math.sin(theta) * force
+        return (fX, fY)
+
+    def update(self, listPlanets: list[Planet]) -> None:
+        totalForce = [sum(i) for i in zip(*[self.attraction(planet) for planet in listPlanets if planet!=self])]
+        self.vel = (self.vel[0]+totalForce[0]/self.mass*self.TIMESTEP,
+                    self.vel[1]+totalForce[1]/self.mass*self.TIMESTEP)
+        self.pos = (self.pos[0]+self.vel[0]*self.TIMESTEP,
+                    self.pos[1]+self.vel[1]*self.TIMESTEP)
 
     def draw(self, screen) -> None:
         x = self.pos[0] * self.SCALE + self.WIDTH / 2
